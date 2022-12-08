f123bf8c6fa1ca207576b42ba49b4ec2208940da
Luisa <marialuisamanzanom@gmail.com>
2022-12-08 11:30:17 -0600

    tiendita

--- MySQL/mySQLs3/tiendita.sql
+++ MySQL/mySQLs3/tiendita.sql
@@ -1,6 +1,44 @@
 show columns from productos;
-INSERT INTO productos (nombre, precio, fecha_alta, idCategoria, existencia)
-VALUES ('Puerquito', 12.5, curdate(), 10, 3);
+INSERT INTO productos (nombre, precio, fecha_alta, idCategoria, existencia, marca)
+VALUES ('Jugo Verduras', 12, curdate(), 8, 33, "V8");
 select * from productos;
-UPDATE productos SET idCategoria = 10 
-WHERE idProducto = 22;
+UPDATE productos SET marca = "Bubaloo" 
+WHERE idProducto = 27;
+show columns from categoria;
+select * from categoria;
+INSERT INTO categoria VALUES (NULL, "Dulces" );
+INSERT INTO categoria (nombre) VALUES ( "Verduras" );
+DELETE FROM categoria WHERE id = 6;
+ALTER TABLE productos ADD COLUMN marca varchar(30) NOT NULL;
+UPDATE productos SET precio = REPLACE (precio, 5, 10); 
+DELETE FROM categoria WHERE id = 13;
+SET FOREIGN_key_checks = 0;
+TRUNCATE TABLE categoria;
+SET FOREIGN_key_checks = 1;
+SELECT * FROM categoria WHERE id < 9;
+SELECT idProducto FROM productos GROUP BY marca;
+INSERT INTO productos VALUES (1,'Refresco de Cola',10.00,'2016-01-01',1,10,'a'),
+(2,'Refesco de limon',9.00,'2015-02-06',1,15,'a'),
+(3,'Limonada',12.00,'2014-05-09',1,12,'a'),
+(4,'Refresco de toronja',9.00,'2016-05-07',1,20,'a'),
+(5,'Agua Mineral',15.00,'2016-02-15',1,8,'a'),
+(6,'Aceite Natural 1 Litro',20.00,'2015-09-20',2,25,'a'),
+(7,'Aceite de Coco 500 ml',15.00,'2016-03-24',2,20,'a'),
+(8,'Jabon lavanda 1 kg',12.00,'2014-02-21',3,6,'a'),
+(9,'Detergente en polvo 1 kg',18.50,'2014-11-17',3,9,'a'),
+(10,'Jabon para manos 500 ml',26.50,'2016-01-10',3,7,'a'),
+(11,'Detergente en polvo 2 kg',30.00,'2014-11-17',3,5,'a'),
+(12,'Detergente en polvo para trastes 500 g',18.50,'2016-02-20',3,8,'a'),
+(13,'Paleta de caramelo',5.00,'2013-01-01',4,50,'a'),
+(14,'Paleta de chocolate',6.50,'2014-05-20',4,34,'a'),
+(15,'Gomitas',12.50,'2016-03-30',4,16,'a'),
+(16,'Bolsa de dulces variados',14.50,'2015-12-12',4,23,'a'),
+(17,'Papas fritas',9.50,'2013-04-09',5,14,'a'),
+(18,'Frituras de queso',8.00,'2014-10-09',5,12,'a'),
+(19,'Papas fristas con limon',10.00,'2015-09-08',5,8,'a'),
+(20,'Bolsa de chicharrones',15.00,'2016-05-20',5,19,'a'),
+(21,'Palomitas de mantequilla',18.00,'2015-11-28',5,12,'a');
+SELECT COUNT(idProducto) AS cantidad, idCategoria FROM productos GROUP BY idCategoria;
+SELECT COUNT(idProducto), idCategoria FROM productos GROUP BY idCategoria;
+SELECT COUNT(existencia), nombre FROM productos GROUP BY nombre;
+
