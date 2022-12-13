f31555dde57a822c01b7db23818d4e033e70cefe
Luisa <marialuisamanzanom@gmail.com>
2022-12-13 11:59:00 -0600

    inyeccion de depenencias corregido

---
+++ java/java-web-1/ejemplo0-1/src/main/java/com/generation/mlmm/app/controller/IndexController.java
@@ -0,0 +1,5 @@
+package com.generation.mlmm.app.controller;
+
+public class IndexController {
+
+}
--- java/java-web-1/ejemplo0-2/src/main/java/com/generation/mlmm/controller/IndexController.java
+++ java/java-web-1/ejemplo0-2/src/main/java/com/generation/mlmm/controller/IndexController.java
@@ -1,14 +1,17 @@
-package com.generation.mlmm.app.controllers;
+package com.generation.mlmm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping ("/")
+@RequestMapping("/")
 public class IndexController {
-	@RequestMapping(value = "2", method = RequestMethod.GET)
-	public String getUserForm() {
-		return "index";
+		@RequestMapping(value="2", method=RequestMethod.GET)
+		public String getUserForm() {
+			
+			return "index";
+		}
 	}
-}
+
+
